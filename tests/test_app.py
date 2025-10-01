e2031b0475e63a238479524df812d7fb00fb6f31
Arrhenius401 <1825270596@qq.com>
2025-10-01 09:47:55 +0800

    fix: 修改test内容，使其匹配app.py现有内容

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,11 @@
-import unittest
-import sys
-from pathlib import Path
-# 将项目根目录添加到Python搜索路径
-sys.path.append(str(Path(__file__).parent.parent))  # 指向flask-web-app目录
-from src.app import app
-class TestApp(unittest.TestCase):
-    def setUp(self):
-        # 创建测试客户端
-        self.client = app.test_client()
-    def test_hello_world(self):
-        # 测试根路由
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('utf-8'), 'Hello,NEU!')
-if __name__ == '__main__':
-    unittest.main()
+import pytest
+from app import app
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+def test_hello(client):
+    response = client.get('/')
+    assert response.data == b"Hello, CI/CD with Jenkins!"
+    assert response.status_code == 200
